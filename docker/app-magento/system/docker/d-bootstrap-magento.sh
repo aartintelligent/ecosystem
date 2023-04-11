4b2e8cab55f04ec166f19eef02a8016f17af027b
aartintelligent <aartintelligent@gmail.com>
2023-04-11 09:29:10 +0200

    initial commit

--- docker/app-magento/system/docker/d-bootstrap-magento.sh
+++ docker/app-magento/system/docker/d-bootstrap-magento.sh
@@ -3,3 +3,10 @@ set -e
 
 (>&2 echo "[*] Bootstrap MAGENTO")
 
+#if [ ! -f "/var/www/app/etc/env.php" ]; then
+#
+#  cp \
+#    "/var/www/app/etc/env.template.php" \
+#    "/var/www/app/etc/env.php"
+#
+#fi
