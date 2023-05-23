9b5bb49817a25795f1a8c44ac956f2eb605b7b96
aartintelligent <aartintelligent@gmail.com>
2023-05-23 11:46:32 +0200

    initial commit

--- docker/app-base/system/docker/d-bootstrap-sstmp.sh
+++ docker/app-base/system/docker/d-bootstrap-sstmp.sh
@@ -7,10 +7,30 @@ while IFS= read -r file; do
 
   for e in "${!SSTMP_@}"; do
 
-      sed -i -e 's!__'"$e"'__!'"$(printenv "$e")"'!g' "$file"
+    sed -i -e 's!__'"$e"'__!'"$(printenv "$e")"'!g' "$file"
 
   done
 
 done < <(find "$directory" -name "*.conf")
+
+touch /etc/ssmtp/ssmtp.conf.tmp
+
+while IFS= read -r line; do
+
+  if [[ "$line" =~ .*=$ ]]; then
+
+    echo "#$line" >> /etc/ssmtp/ssmtp.conf.tmp
+
+    continue
+
+  fi
+
+  echo "$line" >> /etc/ssmtp/ssmtp.conf.tmp
+
+done </etc/ssmtp/ssmtp.conf
+
+mv /etc/ssmtp/ssmtp.conf.tmp /etc/ssmtp/ssmtp.conf
+
+rm -f /etc/ssmtp/ssmtp.conf.tmp
 
 (>&2 echo "[*] Bootstrap SSTMP")
