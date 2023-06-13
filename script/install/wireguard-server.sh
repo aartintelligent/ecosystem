51065786326dedf3d76d2c067a435a9485466a4e
aartintelligent <aurelien.andre@ext.weldom.fr>
2023-06-13 08:45:09 +0200

    initial commit

--- script/install/wireguard-server.sh
+++ script/install/wireguard-server.sh
@@ -6,10 +6,11 @@ SERVER_PORT="51820"
 CLIENTS_SUBNET="10.0.0.0/24"
 INTERFACE="wg0"
 EXTERNAL_INTERFACE="eth0"
+MTU="1420"
 WIREGUARD_DIR="/opt/wireguard"
 
 function helper {
-  echo "Usage: $0 --server-ip <server_ip> [--server-port <server_port>] [--clients-subnet <clients_subnet>] [--interface <interface_name>] [--external-interface <external_interface>]"
+  echo "Usage: $0 --server-ip <server_ip> [--server-port <server_port>] [--clients-subnet <clients_subnet>] [--interface <interface_name>] [--external-interface <external_interface>] [--mtu <mtu>]"
   echo ""
   echo "Options:"
   echo "  --server-ip           The public IP address of the WireGuard server"
@@ -17,6 +18,7 @@ function helper {
   echo "  --clients-subnet      (Optional) The subnet of IP addresses assigned to WireGuard clients (default: 10.0.0.0/24)"
   echo "  --interface           (Optional) The name of the WireGuard network interface (default: wg0)"
   echo "  --external-interface  (Optional) The name of the external network interface (default: eth0)"
+  echo "  --mtu                 (Optional) The MTU (Maximum Transmission Unit) value for the WireGuard interface (default: 1420)"
 }
 
 while [[ $# -gt 0 ]]; do
@@ -48,6 +50,11 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
+    --mtu)
+      MTU="$2"
+      shift
+      shift
+      ;;
     *)
       helper
       exit 1
@@ -61,7 +68,7 @@ if [ -z "$SERVER_IP" ]; then
 fi
 
 if [ "$EUID" -ne 0 ]; then
-  echo "This script must be performed with superuser privileges."
+  echo "This script must be executed with superuser privileges."
   exit 1
 fi
 
@@ -78,16 +85,37 @@ cat << EOF > /etc/wireguard/"$INTERFACE".conf
 PrivateKey = $(cat $WIREGUARD_DIR/private.key)
 Address = $CLIENTS_SUBNET
 ListenPort = $SERVER_PORT
-PostUp = iptables -A FORWARD -i $INTERFACE -j ACCEPT; iptables -t nat -A POSTROUTING -o $EXTERNAL_INTERFACE -j MASQUERADE
-PostDown = iptables -D FORWARD -i $INTERFACE -j ACCEPT; iptables -t nat -D POSTROUTING -o $EXTERNAL_INTERFACE -j MASQUERADE
+MTU = $MTU
+PostUp = $WIREGUARD_DIR/post-up.sh
+PostDown = $WIREGUARD_DIR/post-down.sh
 SaveConfig = true
 
 EOF
 
-echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/10-wireguard.conf
 
-sysctl -p
+sysctl --system
 
 systemctl enable --now "wg-quick@$INTERFACE.service"
+
+cat << EOF > $WIREGUARD_DIR/post-up.sh
+#!/bin/bash
+
+iptables -A FORWARD -i $INTERFACE -j ACCEPT
+iptables -t nat -A POSTROUTING -o $EXTERNAL_INTERFACE -j MASQUERADE
+
+EOF
+
+chmod +x $WIREGUARD_DIR/post-up.sh
+
+cat << EOF > $WIREGUARD_DIR/post-down.sh
+#!/bin/bash
+
+iptables -D FORWARD -i $INTERFACE -j ACCEPT
+iptables -t nat -D POSTROUTING -o $EXTERNAL_INTERFACE -j MASQUERADE
+
+EOF
+
+chmod +x $WIREGUARD_DIR/post-down.sh
 
 echo "The WireGuard server has been successfully installed and configured."
